a1387e633f2e9afdcad0c09c68a685a43d546555
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-30 08:54:48 -0500

    ex 2

--- container_lab.py
+++ container_lab.py
@@ -3,6 +3,15 @@
 # Print out the second student's name.
 # Print out the last student's name.
 
-students = ['David', 'Jquan', 'Iyana', 'Maika']
-print(students[1])
-print(students[2])
+# students = ['David', 'Jquan', 'Iyana', 'Maika']
+# print(students[1])
+# print(students[2])
+
+# Exercise 2
+# Create a tuple named foodscontaining the same number of foods (strings) as there are names in the studentslist.
+# Use a forloop to print out the string "food goes here is a good food".
+
+foods = ('Burger', 'Nachos', 'Cereal', 'Sushi')
+
+for food in foods:
+    print(f'{food} is a gooood food')
