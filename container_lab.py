eaa0383f980e0cfa6ae54c9e8432a8c4d94dbc4b
Raquel Olaya <raquelolaya@Raquels-MacBook-Air.local>
2022-06-30 08:41:38 -0500

    ex 1

---
+++ container_lab.py
@@ -0,0 +1,8 @@
+# Exercise 1
+# Create a list named studentscontaining some student names (strings).
+# Print out the second student's name.
+# Print out the last student's name.
+
+students = ['David', 'Jquan', 'Iyana', 'Maika']
+print(students[1])
+print(students[2])
